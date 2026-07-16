4e02a62f63166371d1cec21c8909f21a2e402095
Yisus2299 <jmzm08@gmail.com>
2026-07-16 01:21:07 -0400

    Implement task validation and commit functionality for pending tasks

--- Router/pending.py
+++ Router/pending.py
@@ -16,3 +16,101 @@ router = APIRouter(
 pending_tasks = {}
 
 FIXED_USER_ID = 1
+
+# GET ENDPOINT:
+
+@router.get("")
+def get_pending_task():
+    
+    user_pending = pending_tasks.get(FIXED_USER_ID, [])
+    
+    return{
+        "user_id": FIXED_USER_ID,
+        "pending_tasks": user_pending,
+        "total": len(user_pending)
+    }
+
+# POST ENDPOINT:
+
+@router.post("", response_model=TaskResponse, status_code=status.HTTP_201_CREATED)
+def add_pending_task(
+    task: TaskCreate,
+    db: Session = Depends(get_db)
+):
+    user_pending = pending_tasks.get(FIXED_USER_ID, [])
+    
+    for existing_task in user_pending:
+        if existing_task["title"].lower() == task.title.lower():
+            raise HTTPException(
+                status_code=status.HTTP_400_BAD_REQUEST,
+                detail= "A pending task with this title already exists"
+            )
+    
+    new_pending_task = {
+        "title":task.title,
+        "description":task.description
+    }
+    user_pending.append(new_pending_task)
+    
+    pending_tasks[FIXED_USER_ID] = user_pending
+    
+    return{
+        "message": "Task added to pending list",
+        "pending_tasks": user_pending
+    }
+    
+# DELETE ENDPOINT:
+
+@router.delete("/{task_index}", status_code=status.HTTP_204_NO_CONTENT)
+def delete_pending_task(task_index:int):
+    user_pending = pending_tasks.get(FIXED_USER_ID, [])
+    
+    if task_index < 0 or task_index >= len(user_pending):
+        raise HTTPException(
+            status_code=status.HTTP_404_NOT_FOUND,
+            detail="Task index out of range"
+        )
+        
+    deleted_task = user_pending.pop(task_index)
+    
+    pending_tasks[FIXED_USER_ID] = user_pending
+    
+    return None
+
+# OPTIONAL ENDPOINT (COMMIT):
+@router.post("/commit", status_code=status.HTTP_201_CREATED)
+def commit_pending_tasks(db: Session = Depends(get_db)):
+  
+    user_pending = pending_tasks.get(FIXED_USER_ID, [])
+    
+    if not user_pending:
+        raise HTTPException(
+            status_code=status.HTTP_400_BAD_REQUEST,
+            detail="No pending tasks to commit"
+        )
+    
+
+    created_tasks = []
+    for pending_task in user_pending:
+
+        task_create = TaskCreate(
+            title=pending_task["title"],
+            description=pending_task.get("description")
+        )
+
+        new_task = create_task_service(
+            db=db,
+            task_create=task_create,
+            user_id=FIXED_USER_ID
+        )
+        created_tasks.append(new_task)
+    
+  
+    pending_tasks[FIXED_USER_ID] = []
+    
+  
+    return {
+        "message": f"{len(created_tasks)} tasks committed successfully",
+        "created_tasks": created_tasks
+    }
+    
